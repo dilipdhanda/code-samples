7c439921eba5968b3114138d007eadc3d141aa96
dilipdhanda <dilipdhanda@hotmail.com>
2025-03-28 13:08:33 -0700

    added gitignore, PessimisticLock, Spring exercises

--- src/main/java/spring/PessimisticLock/ApplicationPessimisticLock.java
+++ src/main/java/spring/PessimisticLock/ApplicationPessimisticLock.java
@@ -1,6 +1,5 @@
 package spring.PessimisticLock;
 
-import jakarta.persistence.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Service;
